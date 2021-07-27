b48fc225e8ae290c4e1bc9e643b46dddb97a45d6
Han <najhan.mnajib@gmail.com>
2021-07-27 14:53:45 +0800

    UI + Report updated

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -12,12 +12,12 @@
             <div class="page-title-box">
                 <div class="page-title-right">
                     <ol class="breadcrumb m-0">
-                        <li class="breadcrumb-item"><a href="javascript: void(0);">Hyper</a></li>
+                        <li class="breadcrumb-item"><a href="javascript: void(0);">KPDNHEP</a></li>
                         <li class="breadcrumb-item"><a href="javascript: void(0);">Apps</a></li>
-                        <li class="breadcrumb-item active">File Manager</li>
+                        <li class="breadcrumb-item active">Reports</li>
                     </ol>
                 </div>
-                <h4 class="page-title">File Manager</h4>
+                <h4 class="page-title">Reports</h4>
             </div>
         </div>
     </div>
